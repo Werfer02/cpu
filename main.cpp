a215efe7702f2666e04f85192d6c5779e3410d18
werfer <jacekjanusiewicz02@gmail.com>
2026-09-19 03:12:20 +0100

    fix lots of errors with ROM operation, fix the reading logic to be better, more versatile, add printError and printInfo utility functions, fix incorrect screenReg size calculation in declaration, fix bug in allocate that wasnt checking if there was enough space when writing a new block into empty memory, add error messagin, return bool to see if allocation succeeded, add skipping printing screen in printScreen if it was never written to, add writeBinaryProgramToFile, make main check if allocating space for program succeeded, add a few more test programs

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <unordered_map>
 #include <array>
 #include <algorithm>
+#include <cctype>
 
 #include "coututils.hpp"
 
@@ -111,8 +112,26 @@ std::unordered_map<operation, operationInfo> operationInfoMap{
 	{operation::GET,        {0b00010000, {operandType::BYTE, operandType::BYTE}}},	// reg, infobytereg (hardcoded info bytes for different possible info to get)
 };
 
+void printError(const std::vector<std::string>& args){
+	std::cout << ansi::bright_red;
+	std::cout << "ERROR: ";
+	for(const std::string& arg : args){
+		std::cout << arg;
+	}
+	std::cout << ansi::reset;
+}
+
+void printInfo(const std::vector<std::string>& args){
+	std::cout << ansi::yellow;
+	std::cout << "INFO: ";
+	for(const std::string& arg : args){
+		std::cout << arg;
+	}
+	std::cout << ansi::reset;
+}
+
 class Sim{
-	static constexpr int HEAP_SIZE = 1*1024;
+	static constexpr int HEAP_SIZE = 1*256;
 
 	static constexpr int SCREEN_W = 32;
 	static constexpr int SCREEN_H = 32;
@@ -125,7 +144,7 @@ class Sim{
 	byte regPixelR = 0;
 	byte regPixelG = 0;
 	byte regPixelB = 0;
-	std::array<std::string, (SCREEN_W * SCREEN_H * 3)> regScreen;
+	std::array<std::string, (SCREEN_W * SCREEN_H)> regScreen;
 	bool screenReady = false;
 
 	byte heap[HEAP_SIZE] = {0};
@@ -155,11 +174,15 @@ public:
 	address allocate(byte bytes){
 		int i = 0;
 		int mergeCandidate = -1;
-		while(i < HEAP_SIZE && i < byte_max){
+		
+		while(i < HEAP_SIZE){
 			if(heap[i] == 0){ // this must mean free, size of a block is never zero, always jumping to size field
-				heap[i] = bytes;
-				heap[i+1] = true;
-				return i+2;
+				if(i + bytes < HEAP_SIZE){
+					heap[i] = bytes;
+					heap[i+1] = true;
+					return i+2;
+				}
+				i++;
 			}
 			else if(heap[i] >= bytes){ // always jumping to size field
 				if(heap[i+1] == false){ // this must mean its free
@@ -185,8 +208,8 @@ public:
 				i += heap[i] + 2; // jump to next size field or free
 			}
 		}
-		std::cerr << "couldn't allocate\n";
-		return -1;
+		printError({"couldnt allocate\n"});
+		return 0;
 	}
 	
 	void free(address tofree){
@@ -210,12 +233,14 @@ public:
 	}
 
 	void run(){
+		std::cout << "< program started >\n";
 		address progCounter = progStart;
-		byte b = heap[progCounter];
-		while(progCounter < HEAP_SIZE && b != operationInfoMap[operation::PROG_END].binary){
-			b = heap[progCounter];
+		while(progCounter < HEAP_SIZE){
+			byte b = heap[progCounter];
+			if (b == operationInfoMap[operation::PROG_END].binary) break;
+
 			if(byteToOperation.find(b) == byteToOperation.end()){
-				std::cerr << ansi::red << "no operation for byte: '" << b << "'" << ansi::reset << "\n";
+				printError({"no operation for byte: '", std::to_string(b), "'\n"});
 				return;
 			}
 			operation op = byteToOperation[b];
@@ -344,6 +369,8 @@ public:
 			}
 			progCounter++;
 		}
+		std::cout << "< program finished >\n";
+
 	}
 
 	byte getInfo(byte infoByte){
@@ -352,7 +379,7 @@ public:
 				return progStart;
 			}
 			default : {
-				std::cerr << ansi::red << "no info for byte: " << infoByte << ansi::reset << "\n";
+				printError({"no info for byte: '", std::to_string((char)infoByte), "'\n"});
 				return 0;
 			}
 		}
@@ -368,7 +395,7 @@ public:
 			case 'G' : return &regPixelG;
 			case 'B' : return &regPixelB;
 			default : {
-				std::cerr << ansi::red << "no register: " << (int)index << ansi::reset << "\n";
+				printError({"no register: '", std::to_string((char)index), "'\n"});
 				return nullptr;
 			}
 		}
@@ -415,6 +442,10 @@ public:
 	}
 
 	void printScreen(){
+		if(!screenReady){
+			printInfo({"screen has not been written to, not printing\n"});
+			return;
+		}
 		for(int i = 0; i < SCREEN_H; i++){
 			for(int j = 0; j < SCREEN_W; j++){
 				std::cout << regScreen[(i*SCREEN_W) + j];
@@ -429,9 +460,9 @@ std::vector<byte> readBinaryProgramFromFile(const std::string& path){
 	std::vector<byte> prog;
 	
 	int length = 0;
-	for(char c; file >> c;){
+	for(char c; c = file.get();){
 		if(length >= byte_max) {
-			std::cerr << ansi::red << "program too long, addressable limit: " << byte_max << ansi::reset << "\n";
+			printError({"program too long, addressable limit: ", std::to_string(byte_max), "\n"});
 			return {0};
 		}
 		else prog.push_back((byte)c);
@@ -439,6 +470,13 @@ std::vector<byte> readBinaryProgramFromFile(const std::string& path){
 	}
 	file.close();
 	return prog;
+}
+
+void writeBinaryProgramToFile(const std::string& path, const std::vector<byte>& prog){
+	std::ofstream file(path, std::ios::binary);
+	
+	file.write(reinterpret_cast<const char*>(prog.data()), prog.size());
+	file.close();
 }
 
 struct instruction {
@@ -459,21 +497,21 @@ std::vector<instruction> readInstructionsFromFile(const std::string& path){
 		linestream.str(line);
 		std::string word;
 
-		if(ROMcounter > 0){
-			linestream >> word;
-			if(word.length() == 1 && !isdigit(word[0])){ // if single non digit char
+		if(ROMcounter > 0){ // if reading rom
+			line = line.substr(1); // skip first char (newline from formatting)
+			if(line.length() == 1 && !isdigit(line[0])){ // if single non digit char
 				instructions[instructions.size() - 1].operands.push_back({operandType::BYTE, (byte)line[0]});
 				ROMcounter--;
 			} else try { 								 // try convert whole line to number
 				size_t numendpos = 0;
-				int num = (byte)std::stoi(word, &numendpos);
-				if(numendpos == word.length()){			 // if whole line consumed as number
+				int num = (byte)std::stoi(line, &numendpos);
+				if(numendpos == line.length()){			 // if whole line consumed as number
 					instructions[instructions.size() - 1].operands.push_back({operandType::BYTE, (byte)num});
 					ROMcounter--;
 				} else throw std::invalid_argument("stoi");
 			}
 			catch (std::invalid_argument){				 // write line byte by byte
-				for(byte c : word){
+				for(byte c : line){
 					instructions[instructions.size() - 1].operands.push_back({operandType::BYTE, c});
 					ROMcounter--;
 				}
@@ -484,7 +522,7 @@ std::vector<instruction> readInstructionsFromFile(const std::string& path){
 				i.op = stringToOperation[word];
 				if(i.op == operation::ROM){
 					if(hasROM){
-						std::cerr << ansi::red << "more than one ROM instruction found " << ansi::reset << "\n";
+						printError({"more than one ROM instruction found\n"});
 						return {};
 					}
 					linestream >> word;
@@ -496,7 +534,7 @@ std::vector<instruction> readInstructionsFromFile(const std::string& path){
 						a = std::stoi(word);
 					}
 					if(a > byte_max){
-						std::cerr << ansi::red << "byte operand too big: " << a << ansi::reset << "\n";
+						printError({"byte operand too big: ", std::to_string(a), "\n"});
 						return {};
 					} else i.operands.push_back({operandType::BYTE, (byte)a});
 
@@ -513,7 +551,7 @@ std::vector<instruction> readInstructionsFromFile(const std::string& path){
 								a = std::stoi(word);
 							}
 							if(a > byte_max){
-								std::cerr << ansi::red << "byte operand too big: " << a << ansi::reset << "\n";
+								printError({"byte operand too big: '", std::to_string(a), "'\n"});
 								return {};
 							} else i.operands.push_back({operandType::BYTE, (byte)a});
 							break;
@@ -527,7 +565,7 @@ std::vector<instruction> readInstructionsFromFile(const std::string& path){
 				instructions.push_back(i);
 			}
 			else {
-				std::cerr << ansi::red << "unknown operation: \"" << word << "\"" << ansi::reset << "\n";
+				printError({"unknown operation: \"", word, "\"\n"});
 			}
 		}
 	}
@@ -545,12 +583,12 @@ std::vector<byte> instructionsToBinary(const std::vector<instruction>& instructi
 	std::unordered_map<int, std::string> indextolabel;
 	for(auto i : instructions){
 		if(position >= byte_max) {
-			std::cerr << ansi::red << "program too long, addressable limit: " << byte_max << ansi::reset << "\n";
+			printError({"program too long, addressable limit: '", std::to_string(byte_max), "'\n"});
 			return {0};
 		}
 		if(i.op == operation::LABEL){
 			if(i.operands.empty()){
-				std::cerr << ansi::red << "empty label" << ansi::reset << "\n";
+				printError({"empty label\n"});
 				return{0};
 			}
 			labelsdefined[i.operands[0].stringvalue] = position;
@@ -562,6 +600,7 @@ std::vector<byte> instructionsToBinary(const std::vector<instruction>& instructi
 			for(int b = 1; b <= i.operands[0].bytevalue; b++){ // skip first operand, use it as length, need the <= !!!
 				prog.push_back(i.operands[b].bytevalue);
 			}
+			position += 2 + i.operands[0].bytevalue; // operation + length byte + length of rom
 			continue;
 		}
 		prog.push_back(operationInfoMap[i.op].binary);
@@ -586,7 +625,7 @@ std::vector<byte> instructionsToBinary(const std::vector<instruction>& instructi
 	}
 	for(auto l : labelsused){
 		if(labelsdefined.find(indextolabel[l.first]) == labelsdefined.end()){
-			std::cerr << ansi::red << "undefined label used: " << l.first << ansi::reset << "\n";
+			printError({"undefined label used: ", std::to_string(l.first), "\n"});
 		} else {
 			prog[l.second] = labelsdefined[indextolabel[l.first]];
 		}
@@ -595,7 +634,7 @@ std::vector<byte> instructionsToBinary(const std::vector<instruction>& instructi
 }
 
 int main(int argc, char** argv){
-	std::string file = "romtest.inst";
+	std::string file = "tests/romtest.inst";
 	if(argc >= 2){
 		file = argv[1];
 	}
@@ -604,10 +643,15 @@ int main(int argc, char** argv){
 	std::vector<byte> program = instructionsToBinary(instructions);
 
 	address a = s.allocate(program.size()*sizeof(byte));
-	s.write(program.data(), a, program.size() * sizeof(byte));
-	s.setProgStart(a);
-	//s.printHeap();
-	s.run();
-	s.printScreen();
-	//s.printRegisters();
+	if(a){
+		s.write(program.data(), a, program.size() * sizeof(byte));
+		s.setProgStart(a);
+		s.printHeap();
+		s.run();
+		s.printScreen();
+		std::cout << "registers:\n";
+		s.printRegisters();
+	} else{
+		printError({"error allocating, got address 0\n"});
+	}
 }
